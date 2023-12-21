8041c10151cc7f83a792bbbfef3d7ed29d89ad76
Nipunikumudika <109246366+Nipunikumudika@users.noreply.github.com>
2023-12-21 11:53:39 +0530

    Data Memory
    Creating Data Memory and tb

---
+++ DataMemory_tb.v
@@ -0,0 +1,23 @@
+`timescale 1ms/1ms
+
+module DataMemory_tb();
+  reg [31:0] address, write_data;
+  reg mem_write, mem_read;
+  wire [31:0] read_data;
+
+  DataMemory dut (
+    .Address(address),
+    .Write_Data(write_data),
+    .Mem_Write(mem_write),
+    .Mem_Read(mem_read),
+    .Read_Data(read_data)
+  );
+
+  initial begin
+    #10 address = 3; write_data = 150; mem_write = 1; mem_read = 0;
+    #10 address = 3; mem_write = 0; mem_read = 1;
+    #10;
+  end
+
+endmodule
+
