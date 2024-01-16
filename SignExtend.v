efe7348737c3505862e074045d9fe5f87827fd41
Bhanuka Anjana <bhanuka2500@gmail.com>
2024-01-17 03:08:53 +0530

    Implement sign extended module

---
+++ SignExtend.v
@@ -0,0 +1,12 @@
+//Implimentation of the sign extend module
+module SignExtend(
+    // Inputs
+    input [15:0] Inst,
+    // Outputs
+    output reg [31:0] Extended
+);
+
+always @(Inst) begin
+    Extended = {16'b0000000000000000, Immediate};
+end
+endmodule
