b7d40cdb9f770bbcd5689b30684d0270620e6c9e
Bhanuka Anjana <bhanuka2500@gmail.com>
2024-01-16 22:40:46 +0530

    implement ALU module

---
+++ ALU.v
@@ -0,0 +1,24 @@
+//ALU implimentation
+module ALU(
+    // Inputs
+    input [31:0] A,B;
+    input [3:0] ALUControl;
+
+    // Outputs
+    output reg [31:0] ALUOut;
+    output reg Zero; // Zero flag
+);
+
+    always @* begin
+        case(ALUControl)
+            4'b0000: ALUOut <= A + B; // add
+            4'b0001: ALUOut <= A - B; // sub
+            4'b0010: ALUOut <= A & B; // and
+            4'b0011: ALUOut <= A | B; // or
+            4'b0100: ALUOut <= ~(A | B); // not
+            4'b1000: ALUOut <= A < B; // slt
+            4'b1001: ALUOut <= A < B; // sltu
+            default: ALUOut <= 0;
+        endcase
+        Zero <= (ALUOut == 0);
+    end
