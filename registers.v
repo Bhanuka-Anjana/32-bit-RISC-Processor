c36d95fa145b1703bd2e138d61c1f2371900ffe5
Bhanuka Anjana <85387483+Bhanuka-Anjana@users.noreply.github.com>
2024-01-16 23:12:03 +0530

    Revert "Alu & ALU-TB"

--- registers.v
+++ registers.v
@@ -1,22 +1,22 @@
 module registers (
-    input [4:0] Read_Register_1, Read_Register_2, Write_Register, // 5-bit register address 
-    input [31:0] Write_Data, // data to be written to the register
-    input Sig_Reg_Write, // set to 1 if register is to be written, 0 otherwise
-    output reg [31:0] Read_Data_1, Read_Data_2 // data read from register
+    input [4:0] read_register_1, read_register_2, write_register, // 5-bit register address 
+    input [31:0] write_data, // data to be written to the register
+    input sig_reg_write, // set to 1 if register is to be written, 0 otherwise
+    output reg [31:0] read_data_1, read_data_2 // data read from register
     );
 
-    reg [31:0] Registers [0:31]; // 32 registers
+    reg [31:0] registers [0:31]; // 32 registers
 
-    always @(Write_Register, Write_Data) begin
-        if (Sig_Reg_Write) begin
-            Registers[Write_Register] <= Write_Data;
+    always @(write_register, write_data) begin
+        if (sig_reg_write) begin
+            registers[write_register] <= write_data;
         end
-    end
+     end
 
-    always @(Read_Register_1, Read_Register_2) begin
-        if (!Sig_Reg_Write) begin
-            Read_Data_1 = Registers[Read_Register_1];
-            Read_Data_2 = Registers[Read_Register_2];
+    always @(read_register_1, read_register_2) begin
+        if (!sig_reg_write) begin
+            read_data_1 = registers[read_register_1];
+            read_data_2 = registers[read_register_2];
         end
     end
 endmodule
